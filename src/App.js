2045110b99f5a07a078884aa3d12466b16461397
Sarah Adlington <cheshire6655@icloud.com>
2021-04-10 08:31:34 +0100

    Remove logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           This is going to be my portfolio
         </p>
